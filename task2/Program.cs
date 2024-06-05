840fe0648ff1c1dbae183f549e80ed7e2fbed9d8
Kovalev Alexandr <kovalew.a666@gmail.com>
2024-06-05 15:30:07 +0300

    lesson1 task2

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿int a1 = 5;
+int a2 = 6;
+int sum = a1+a2;
+Console.WriteLine(sum);
+
+long b1 = 6;
+long sum2 = a1+b1;
+Console.WriteLine(sum2);
+
+double c=5.5;
+double sum3 = c+a1;
+Console.WriteLine(sum3);
+
+int product1 = a1*a2;
+long product2 = b1*a1;
+double product3 = c*a1!;
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
