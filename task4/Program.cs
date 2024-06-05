902f60840b5c19b3d8cdcca16f2f82dd4346ea07
Kovalev Alexandr <kovalew.a666@gmail.com>
2024-06-05 16:47:14 +0300

    5 гирь

---
+++ task4/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 13;
+int b = 20;
+int c = 45;
+int d = 100;
+int e = 10;
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
